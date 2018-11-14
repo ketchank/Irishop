403bacc5917824ded8497ae8dcbfc1750f05ec69
ketchankoue <stephaneketchankoue@gmail.com>
2018-11-14 17:47:27 +0100

    Update app/Http/Controllers/HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Role::create(['name' => 'writer']);
+        Permission::create(['name' => 'edit articles']);
+        return view('pages.intro');
     }
 }
